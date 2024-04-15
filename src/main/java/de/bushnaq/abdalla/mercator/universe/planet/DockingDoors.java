94b3670e7d690a1af7fa18def047c166cdbbf6db
kunterbunt2 <abdalla@bushnaq.de>
2024-04-15 17:56:58 +0200

    Added docking door sound.

--- src/main/java/de/bushnaq/abdalla/mercator/universe/planet/DockingDoors.java
+++ src/main/java/de/bushnaq/abdalla/mercator/universe/planet/DockingDoors.java
@@ -16,15 +16,25 @@
 
 package de.bushnaq.abdalla.mercator.universe.planet;
 
+import com.badlogic.gdx.Gdx;
 import de.bushnaq.abdalla.engine.RenderEngine3D;
+import de.bushnaq.abdalla.engine.audio.OggPlayer;
+import de.bushnaq.abdalla.engine.audio.OpenAlException;
+import de.bushnaq.abdalla.mercator.engine.AtlasManager;
 import de.bushnaq.abdalla.mercator.engine.GameEngine3D;
 
 import java.util.ArrayList;
 
 public class DockingDoors extends ArrayList<DockingDoor> {
+    private final float[]   position = new float[3];
+    private       OggPlayer oggPlayer;
+
     public DockingDoors(Planet planet) {
         add(new DockingDoor(planet, planet.getName().substring(0, 2), -32f, 0));
         add(new DockingDoor(planet, planet.getName().substring(2, planet.getName().length()), 32f, 0));
+        position[0] = planet.x;
+        position[1] = planet.y;
+        position[2] = planet.z;
     }
 
     public void advanceInTime(float realTimeDelta) {
@@ -37,11 +47,33 @@ public class DockingDoors extends ArrayList<DockingDoor> {
         for (DockingDoor dockingDoor : this) {
             dockingDoor.create(renderEngine);
         }
+        try {
+            oggPlayer = renderEngine.getGameEngine().audioEngine.createAudioProducer(OggPlayer.class);
+            oggPlayer.setFile(Gdx.files.internal(AtlasManager.getAssetsFolderName() + "/audio/docking-door.ogg"));
+            oggPlayer.setGain(200.0f);
+            oggPlayer.setAmbient(false);
+            oggPlayer.setLoop(true);
+        } catch (OpenAlException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void render(RenderEngine3D<GameEngine3D> renderEngine) {
+    public void render(RenderEngine3D<GameEngine3D> renderEngine) throws OpenAlException {
         for (DockingDoor dockingDoor : this) {
             dockingDoor.render(renderEngine);
+        }
+        oggPlayer.setPositionAndVelocity(position, null);
+        if (renderEngine.getCamera().position.dst(position[0], position[1], position[2]) < 1000) {
+            switch (get(0).getDockingDoorState()) {
+                case OPEN, CLOSED -> {
+                    oggPlayer.pause();
+                }
+                case LOWERING, CLOSING, RAISING, OPENING -> {
+                    oggPlayer.play();
+                }
+            }
+        } else {
+            oggPlayer.pause();
         }
     }
 
